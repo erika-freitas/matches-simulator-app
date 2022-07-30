10b0f57cf0da8e10ff2509c9ab67b5297f77fa12
Erika Freitas <erikamsfreitas@gmail.com>
2022-07-30 18:56:18 -0300

    Apply viewBiding

--- app/src/main/java/ef/simulator/MainActivity.kt
+++ app/src/main/java/ef/simulator/MainActivity.kt
@@ -1,11 +1,17 @@
 package ef.simulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ef.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
