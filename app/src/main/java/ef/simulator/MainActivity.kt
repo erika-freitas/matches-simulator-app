2c3b6fe763785e7094121fd80f6b2dfbae0b3222
Erika Freitas <erikamsfreitas@gmail.com>
2022-07-30 16:41:54 -0300

    first commit

---
+++ app/src/main/java/ef/simulator/MainActivity.kt
@@ -0,0 +1,11 @@
+package ef.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
